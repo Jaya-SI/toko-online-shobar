329e398f935d0ddec8b0b794c4f038acc5e4794d
Jaya-SI <60172081+Jaya-SI@users.noreply.github.com>
2022-04-29 04:29:40 +0800

    tambah assets dan chat empty

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -5,13 +5,88 @@ import 'package:shopbar/theme.dart';
 class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Chat Page',
-        style: GoogleFonts.poppins(
-          color: waranaPutih,
+    Widget header() {
+      return AppBar(
+        backgroundColor: warnaHitam1,
+        title: Text(
+          'Message Support',
+          style: GoogleFonts.poppins(
+            fontSize: 18,
+            fontWeight: medium,
+            color: waranaPutih,
+          ),
         ),
-      ),
+        elevation: 0,
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+      );
+    }
+
+    Widget content() {
+      return Expanded(
+        child: Container(
+          color: warnaHitam3,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/icon_headset.png',
+                width: 80,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Opss no message yet?',
+                style: GoogleFonts.poppins(
+                  color: waranaPutih,
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                'You have never done a transaction',
+                style: GoogleFonts.poppins(
+                  color: abuText,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                height: 44,
+                child: TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    backgroundColor: warnaUngu,
+                  ),
+                  child: Text(
+                    'Explore Store',
+                    style: GoogleFonts.poppins(
+                      fontSize: 16,
+                      fontWeight: medium,
+                      color: waranaPutih,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
