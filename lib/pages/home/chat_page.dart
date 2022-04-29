7c8222bc9b174ffa521751be56397f0b70a472f7
Jaya-SI <60172081+Jaya-SI@users.noreply.github.com>
2022-04-29 08:31:53 +0800

    chat_tile

--- lib/pages/home/chat_page.dart
+++ lib/pages/home/chat_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shopbar/theme.dart';
+import 'package:shopbar/widgets/chat_tile.dart';
 
 class ChatPage extends StatelessWidget {
   @override
@@ -22,60 +23,82 @@ class ChatPage extends StatelessWidget {
       );
     }
 
+    Widget emptyChat() {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/icon_headset.png',
+            width: 80,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Opss no message yet?',
+            style: GoogleFonts.poppins(
+              color: waranaPutih,
+              fontSize: 16,
+              fontWeight: medium,
+            ),
+          ),
+          SizedBox(
+            height: 12,
+          ),
+          Text(
+            'You have never done a transaction',
+            style: GoogleFonts.poppins(
+              color: abuText,
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 44,
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                backgroundColor: warnaUngu,
+              ),
+              child: Text(
+                'Explore Store',
+                style: GoogleFonts.poppins(
+                  fontSize: 16,
+                  fontWeight: medium,
+                  color: waranaPutih,
+                ),
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
     Widget content() {
       return Expanded(
         child: Container(
           color: warnaHitam3,
           width: double.infinity,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: ListView(
+            padding: EdgeInsets.symmetric(horizontal: 30),
             children: [
-              Image.asset(
-                'assets/icon_headset.png',
-                width: 80,
+              Column(
+                children: [
+                  ChatTile(),
+                  ChatTile(),
+                  ChatTile(),
+                  ChatTile(),
+                  ChatTile(),
+                  ChatTile(),
+                  ChatTile(),
+                  ChatTile(),
+                  ChatTile(),
+                  ChatTile(),
+                ],
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Opss no message yet?',
-                style: GoogleFonts.poppins(
-                  color: waranaPutih,
-                  fontSize: 16,
-                  fontWeight: medium,
-                ),
-              ),
-              SizedBox(
-                height: 12,
-              ),
-              Text(
-                'You have never done a transaction',
-                style: GoogleFonts.poppins(
-                  color: abuText,
-                ),
-              ),
-              SizedBox(height: 20),
-              Container(
-                height: 44,
-                child: TextButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 24, vertical: 10),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    backgroundColor: warnaUngu,
-                  ),
-                  child: Text(
-                    'Explore Store',
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      fontWeight: medium,
-                      color: waranaPutih,
-                    ),
-                  ),
-                ),
-              )
             ],
           ),
         ),
