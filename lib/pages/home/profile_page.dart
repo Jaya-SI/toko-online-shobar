6f368bb0de53b0f842a190ac20253d06854432f0
Jaya-SI <60172081+Jaya-SI@users.noreply.github.com>
2022-05-03 07:20:41 +0800

    penambahan content di profile_page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -57,9 +57,82 @@ class ProfilePage extends StatelessWidget {
       );
     }
 
+    menuItem(String text) {
+      return Container(
+        margin: EdgeInsets.only(top: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              text,
+              style: GoogleFonts.poppins(
+                fontSize: 13,
+                color: abuText2,
+              ),
+            ),
+            Icon(
+              Icons.chevron_right,
+              color: abuText2,
+            ),
+          ],
+        ),
+      );
+    }
+
+    content() {
+      return Expanded(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          decoration: BoxDecoration(
+            color: warnaHitam3,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Account',
+                style: GoogleFonts.poppins(
+                  color: waranaPutih,
+                  fontSize: 16,
+                  fontWeight: semibold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              menuItem('Edit Profile'),
+              menuItem('Your Orders'),
+              menuItem('Help'),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                'Account',
+                style: GoogleFonts.poppins(
+                  color: waranaPutih,
+                  fontSize: 16,
+                  fontWeight: semibold,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              menuItem('Privacy & Policy'),
+              menuItem('Term of Service'),
+              menuItem('Rate App'),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Column(
       children: [
         header(),
+        content(),
       ],
     );
   }
