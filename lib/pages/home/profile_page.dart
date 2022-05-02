aa4b57f40fec7beeb0eabbf9133451f4db8cd0d4
Jaya-SI <60172081+Jaya-SI@users.noreply.github.com>
2022-05-03 06:54:43 +0800

    header di profile_page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -5,13 +5,62 @@ import 'package:shopbar/theme.dart';
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile Page',
-        style: GoogleFonts.poppins(
-          color: waranaPutih,
+    header() {
+      return AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: warnaHitam1,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(30),
+            child: Row(
+              children: [
+                ClipOval(
+                  child: Image.asset(
+                    'assets/image_profile.png',
+                    width: 64,
+                  ),
+                ),
+                SizedBox(
+                  width: 16,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hallo, Jaya',
+                        style: GoogleFonts.poppins(
+                          fontSize: 24,
+                          fontWeight: semibold,
+                          color: waranaPutih,
+                        ),
+                      ),
+                      Text(
+                        '@jaya.saptr',
+                        style: GoogleFonts.poppins(
+                          fontSize: 16,
+                          color: abuText,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  'assets/button_exit.png',
+                  width: 20,
+                ),
+              ],
+            ),
+          ),
         ),
-      ),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
