9a522dd98132ce6868c01b5cbb94799e8ce24df0
Jaya-SI <60172081+Jaya-SI@users.noreply.github.com>
2022-05-05 14:21:45 +0800

    product page content

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:shopbar/theme.dart';
 
 class ProductPage extends StatefulWidget {
@@ -92,11 +93,116 @@ class _ProductPageState extends State<ProductPage> {
       );
     }
 
+    content() {
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(top: 17),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(24),
+          ),
+          color: warnaHitam1,
+        ),
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 30, left: 30, right: 30),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Sepatu Adidas Anjay',
+                          style: GoogleFonts.poppins(
+                            fontSize: 18,
+                            fontWeight: medium,
+                            color: waranaPutih,
+                          ),
+                        ),
+                        Text(
+                          'Hiking',
+                          style: GoogleFonts.poppins(
+                            fontSize: 12,
+                            color: abuText2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    'assets/button_whislist.png',
+                    width: 46,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20, left: 30, right: 30),
+              padding: EdgeInsets.all(16),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                color: warnaHitam2,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Price starts from',
+                    style: GoogleFonts.poppins(
+                      color: waranaPutih,
+                    ),
+                  ),
+                  Text(
+                    '\$143,98',
+                    style: GoogleFonts.poppins(
+                      color: warnaBiru,
+                      fontSize: 16,
+                      fontWeight: semibold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 30, left: 30, right: 30),
+              width: double.infinity,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Description',
+                    style: GoogleFonts.poppins(
+                      fontWeight: medium,
+                      color: waranaPutih,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    'Unpaved trails and mixed surfaces are easy when you have the traction and support you need. Casual enough for the daily commute.',
+                    style: GoogleFonts.poppins(
+                      color: abuText,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: backgroud1,
       body: ListView(
         children: [
           header(),
+          content(),
         ],
       ),
     );
